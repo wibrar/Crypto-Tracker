be253c6c95501d4d7f2b8aabfbe8589b62a4978e
Wasif Ibrar <wibrar@mun.ca>
2025-08-09 22:31:40 -0230

    Header Component Build

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
     <div>
-      <Headers />
+      <Header/>
     </div>
     </BrowserRouter>
   );
--- crypto-tracker/src/components/Header.js
+++ crypto-tracker/src/components/Header.js
@@ -1,1 +1,11 @@
-rafce
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        Header
+    </div>
+  )
+}
+
+export default Header
