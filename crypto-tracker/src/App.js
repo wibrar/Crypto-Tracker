7088729fc7bfe731cc05b5f1a5754186ce2acecd
Wasif Ibrar <wibrar@mun.ca>
2025-08-09 23:47:30 -0230

    Header is almost done!
    Changed Router version to v5, used Material UI to build the header.

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import Header from './components/Header';
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: '#14161a',
+    color: 'white',
+    minHeight: '100vh',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-    <div>
-      <Header/>
-    </div>
+      <div className={classes.App}>
+        <Header />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+      </div>
     </BrowserRouter>
   );
 }
--- crypto-tracker/src/components/Header.js
+++ crypto-tracker/src/components/Header.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import { AppBar, Container, createTheme, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: 'gold',
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  },
+  appBar: {
+  },
+}));
 const Header = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const darktheme = createTheme({
+    palette: {
+      primary: {  
+        main: '#fff',
+      },
+      type: 'dark',
+    },
+  });
+
   return (
-    <div>
-        Header
-    </div>
+    <ThemeProvider theme={darktheme}>
+        <AppBar color= 'transparent' position='static'>
+          <Container>
+            <Toolbar>
+              <Typography onClick = {()=> history.push('/')} className={classes.title} variant='h6'>Crypto Tracker</Typography>
+              <Select variant='outlined'
+                style={{
+                  width: 100,
+                  height: 40,
+                  marginRight: 15,
+                }}>
+                  <MenuItem value={'USD'} >USD</MenuItem>
+                  <MenuItem value={'CAD'}>CAD</MenuItem>
+                </Select>
+            </Toolbar>
+          </Container>
+        </AppBar>
+    </ThemeProvider>
   )
 }
 
